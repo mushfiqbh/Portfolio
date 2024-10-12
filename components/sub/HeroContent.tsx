ae4d07d223109bab4ddae4affa03913460ce6a79
mushfiqbh <mushfiqbh@gmail.com>
2024-10-12 13:45:41 +0600

    Update HeroContent

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -53,7 +53,7 @@ const HeroContent = () => {
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
-          Learn More!
+          Hire Me!
         </motion.a>
       </div>
 
